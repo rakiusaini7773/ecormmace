6ce40ee2b92158cb9842fa7127163c97692a8e26
ravindra.s@singhsoft.com <ravindra.s@singhsoft.com>
2025-06-13 16:28:01 +0530

    add

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,28 +1,39 @@
-// AllRoutes.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+
+// Pages (Public)
 import { Landing } from '../pages/Landing';
 import { Product } from '../pages/Product';
 import ProductDetailPage from '../pages/ProductDetailPage';
 import { About } from '../pages/About';
 import LoginPage from '../components/LoginPage';
-import Layout from './Layout';
+
+// Pages (Admin)
 import AddBannerForm from '../pages/AddBannerForm';
 import AddCategoryForm from '../pages/AddCategoryForm';
 import AddProductForm from '../pages/AddProductForm';
 
+// Layouts & Route Protection
+import Layout from './Layout';
+import ProtectedRoute from '../networking/ProtectedRoute';
 
 export const AllRoutes = () => {
   return (
     <Routes>
+      {/* 🌐 Public Routes */}
       <Route path="/" element={<Landing />} />
       <Route path="/product" element={<Product />} />
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/product/:id" element={<ProductDetailPage />} />
       <Route path="/about" element={<About />} />
-      <Route element={<Layout />}>
-        <Route path="/admin/banner" element={<AddBannerForm />} />
-         <Route path="/admin/categories" element={<AddCategoryForm />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      {/* 🔐 Protected Admin Routes */}
+      <Route element={<ProtectedRoute allowedRoles={['admin']} />}>
+        <Route element={<Layout />}>
+          <Route path="/admin/banner" element={<AddBannerForm />} />
+          <Route path="/admin/categories" element={<AddCategoryForm />} />
           <Route path="/admin/products" element={<AddProductForm />} />
+        </Route>
       </Route>
     </Routes>
   );
